be32afc3a94beb4e82e8451f46fbdd5402db4c29
Coding_Seal <rodion2023@mail.ru>
2022-10-13 22:12:48 +0300

    Lab_3 finished

--- Lab_3/task_4.bash
+++ Lab_3/task_4.bash
@@ -1,1 +1,7 @@
-{ ls file1 file2 2>&1 >&3 | cat -b >file1; } 3>&1
+{ ls file1 file2 2>&1 >&3 3>&- | cat -b >file1 ; } 3>&1
+
+
+# Command grouping
+
+# When commands are grouped, redirections may be applied 
+# to the entire command list.

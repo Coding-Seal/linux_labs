b8880b36349897afba69c2263b10731de7041e9c
Coding_Seal <rodion2023@mail.ru>
2022-10-14 10:01:47 +0300

    Patch on Lab_3

--- Lab_2/user.job.bash
+++ Lab_2/user.job.bash
@@ -1,1 +1,1 @@
-find -name "*.txt" -exec grep -Pio "(?<=\s|:|\.|\(|^)[a-z0-9]+([\.-][a-z0-9]+)*@([a-z0-9]+\.)+[a-z0-9]+(?=\s|$|\.|,|\))" '{}' \; | tr [A-Z] [a-z] | sort -u >> allemails.lst   
+find -name "*.txt" -exec grep -Pio "(?<=\s|:|\.|\(|^)[a-z][a-z0-9]+([\.-][a-z0-9]+)*@([a-z0-9]+)([\.-][a-z0-9]+)*(\.[a-z0-9]+)(?=\s|$|\.|,|\))" '{}' \; | tr [A-Z] [a-z] | sort -u >> allemails.lst   
